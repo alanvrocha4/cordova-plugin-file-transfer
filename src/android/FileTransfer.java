b9c3f10e1f03ab3707670463c3285be800bfdf8a
alanvrocha4 <alanv.rocha4@gmail.com>
2016-09-27 17:57:28 -0300

    Update FileTransfer.java

--- src/android/FileTransfer.java
+++ src/android/FileTransfer.java
@@ -948,7 +948,8 @@ public class FileTransfer extends CordovaPlugin {
                                        //context.startActivity(intent);
                                    
                                 Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                                mediaScanIntent.setData(fileEntry.get("nativeURL"));
+                                String url = fileEntry.get("nativeURL");
+                                mediaScanIntent.setData(url);
                                 context.sendBroadcast(mediaScanIntent);
                                 result = new PluginResult(PluginResult.Status.OK, fileEntry);
                             } else {
